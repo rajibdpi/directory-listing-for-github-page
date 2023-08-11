e2ca25f585b2413c711bc8e0a167807cc42543a2
Rajib Ahmed <rajibdpi@gmail.com>
2023-08-12 02:27:20 +0600

    Updatd

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -2,10 +2,10 @@
 
 sudo apt-get update
 sudo apt-get install cmake python3 -y
-git clone --depth 1 https://github.com/jayanta525/apindex-v2.git
-cd apindex-v2
+git clone --depth 1 https://github.com/rajibdpi/directory-listing-for-github-page.git
+cd directory-listing-for-github-page
 cmake . -DCMAKE_INSTALL_PREFIX=/usr
 sudo make install
 cd ..
-rm -rf apindex-v2/
-apindex $1/.
+rm -rf directory-listing-for-github-page/
+directory-listing-for-github-page $1/.
