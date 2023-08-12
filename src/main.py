8773c794d43eeefec677fd7b5a8bb888c4afb554
Rajib Ahmed <rajibdpi@gmail.com>
2023-08-12 14:02:39 +0600

    Updated

--- src/main.py
+++ src/main.py
@@ -53,20 +53,24 @@ def main():
                 # dirnames.sort(key=int)
                 dirnames.sort()
                 for subdirname in dirnames:
-                    f.write(
-                        '<tr class="w-1/4 bg-white border-b hover:bg-gray-50"><th scope="row" class=" py-2 px-2 lg:px-6 font-medium text-gray-900 whitespace-nowrap flex align-middle"><img style="max-width:23px; margin-right:5px" src="'
-                        + get_icon_base64("o.folder")
-                        + '"/>'
-                        + '<a class="my-auto text-blue-700" href="'
-                        + subdirname
-                        + '/">'
-                        + subdirname
-                        + "/</a></th><td>-</td><td>-</td></tr>\n"
-                    )
+                    if not subdirname.startswith("."):
+                        yield subdirname
+                        f.write(
+                            '<tr class="w-1/4 bg-white border-b hover:bg-gray-50"><th scope="row" class=" py-2 px-2 lg:px-6 font-medium text-gray-900 whitespace-nowrap flex align-middle"><img style="max-width:23px; margin-right:5px" src="'
+                            + get_icon_base64("o.folder")
+                            + '"/>'
+                            + '<a class="my-auto text-blue-700" href="'
+                            + subdirname
+                            + '/">'
+                            + subdirname
+                            + "/</a></th><td>-</td><td>-</td></tr>\n"
+                        )
                 # sort filenames alphabetically
                 # filenames.sort(key=int)
                 filenames.sort()
                 for filename in filenames:
+                    if not filename.startswith("."):
+                        yield filename
                     path = dirname == "." and filename or dirname + "/" + filename
                     f.write(
                         '<tr class="w-1/4 bg-white border-b hover:bg-gray-50"><th scope="row" class=" py-2 px-2 lg:px-6 font-medium text-gray-900 whitespace-nowrap flex align-middle"><img style="max-width:23px; margin-right:5px" src="'
@@ -82,7 +86,6 @@ def main():
                         + get_file_modified_time(path)
                         + "</td></tr>\n"
                     )
-
                 f.write(
                     "\n".join(
                         [
